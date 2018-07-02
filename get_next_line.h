e19dd781f6a82343ffa8c85c89b475534cc1ca58
Andrii HRYHORYSHYN <ahryhory@e3r2p5.unit.ua>
2018-07-02 19:11:51 +0300

    get_line

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahryhory <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/23 13:34:25 by ahryhory          #+#    #+#             */
+/*   Updated: 2018/02/18 14:52:54 by ahryhory         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 150
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
